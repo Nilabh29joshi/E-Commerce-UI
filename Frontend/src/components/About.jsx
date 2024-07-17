1aa91cf7ddde524d708117cd0c3ed3efed78d75e
nilabh joshi <nilabhrrr@gmail.com>
2024-07-17 18:54:55 +0530

    first commit

---
+++ Frontend/src/components/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we're serious about is food.</p>
+            </div>
+            <p className="mid">
+            At NJFOODS, we believe dining is an experience. We strive to create a warm and inviting atmosphere where you can relax, connect with loved ones, and savor the delicious food we prepare with care. We take care of our customers and we have friendly staff, live music nights, family-friendly environment.
+            </p>
+            <Link to={"/"}>
+              Explore Menu{" "}
+              <span>
+                <HiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
